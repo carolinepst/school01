f4e23506615614793265a8f3aa197d60898c5618
carolinepst <carolinepasti854@gmail.com>
2025-10-04 22:29:00 +0700

    menambahkan isi kelas Student
    # Conflicts:
    #	src/course/Student.kt

--- src/course/Student.kt
+++ src/course/Student.kt
@@ -1,16 +1,16 @@
 package course
-
-import java.net.InetAddress
-
 class Student(
     name: String,
-    var studentid: String,
     address: String,
-    phoneNumber: String
-) : Person(name, address, phoneNumber){
+    phoneNumber: String,
+    var studentId: String,
+    var major: String
+) : Person(name, address, phoneNumber) {
+
+
     override fun displayInfo() {
         super.displayInfo()
-        println("Student ID: $studentid")
+        println("Student ID: $studentId")
+        println("Major: $major")
     }
 }
-
