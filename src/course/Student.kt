621745d9ced8059b5598ddaa2ee5d6e0f95890ec
EsGus <subarukun65@gmail.com>
2025-10-05 01:27:15 +0700

    kelas course beserta packagenya

--- src/course/Student.kt
+++ src/course/Student.kt
@@ -1,4 +1,16 @@
 package course
 
-class Student {
+import java.net.InetAddress
+
+class Student(
+    name: String,
+    var studentid: String,
+    address: String,
+    phoneNumber: String
+) : Person(name, address, phoneNumber){
+    override fun displayInfo() {
+        super.displayInfo()
+        println("Student ID: $studentid")
+    }
 }
+
---
+++ src/course/StudentOrganisation.kt
@@ -0,0 +1,25 @@
+package course
+
+class StudentOrganisation (
+    var leaders: MutableList<Student>
+){
+    fun addLeader(student: Student){
+        leaders.add(student)
+    }
+
+    fun removeLeader(student: Student){
+        leaders.remove(student)
+    }
+
+    fun displayInfo(){
+        println("=== Student Organisation ===")
+        if (leaders.isEmpty()){
+            println("Belum ada pengurus.")
+        } else {
+            println("Daftar Pengurus")
+            leaders.forEach { leader ->
+                println("- ${leader.name} (ID: ${leader.studentid})")
+            }
+        }
+    }
+}
